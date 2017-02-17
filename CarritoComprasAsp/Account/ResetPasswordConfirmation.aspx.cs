6bc23f0b2cf4859becff556aaa1c90a57eb0b973
Jorge <Jorge@ASUSCRACK>
2017-02-17 10:34:53 -0600

    Agregar archivos de proyecto.

---
+++ CarritoComprasAsp/Account/ResetPasswordConfirmation.aspx.cs
@@ -0,0 +1,8 @@
+﻿using System.Web.UI;
+
+namespace CarritoComprasAsp.Account
+{
+    public partial class ResetPasswordConfirmation : Page
+    {
+    }
+}
---
+++ CarritoComprasAsp/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CarritoComprasAsp.Startup))]
+namespace CarritoComprasAsp
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
